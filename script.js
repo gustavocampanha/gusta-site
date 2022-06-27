b9d59843c84d4c1c647d6654b3f7a077a6a74401
Gustavo Campanha <gustavo.gcf@outlook.com>
2022-06-27 15:14:17 -0300

    Crie botao close e send modal box

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-const open = document.getElementById('open');
+const open_header = document.getElementById('open_header');
+const open_contact = document.getElementById('open_contact');
 const modal_box = document.getElementById('modal-box');
 const close = document.getElementById('close');
+const send = document.getElementById('send');
+open_header.addEventListener('click', () => {
+    modal_box.classList.add('show');
+});
 
-open.addEventListener('click', () => {
+open_contact.addEventListener('click', () => {
     modal_box.classList.add('show');
 });
 
 close.addEventListener('click', () => {
     modal_box.classList.remove('show');
+    document.getElementById("info_pessoa_name").value = "";
+    document.getElementById("info_pessoa_email").value = "";
+    document.getElementById("info_pessoa_phone").value = "";
+});
+
+send.addEventListener('click', () => {
+    modal_box.classList.remove('show');
+    document.getElementById("info_pessoa_name").value = "";
+    document.getElementById("info_pessoa_email").value = "";
+    document.getElementById("info_pessoa_phone").value = "";
 });
